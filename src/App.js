867df7f454af76926560fe838796dd9ca6592d1a
Emily Renegar <egrenegar@gmail.com>
2020-07-03 11:40:54 -0400

    removed unneccessary imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import Header from './components/Header';
-// import Table from './components/Table';
 import './App.css';
 import EmployeeContainer from './components/EmployeeContainer';
 
